dbebb26bcea667d244c9406c03d0a95f02b6180f
dreamxinxcode <dreamxinxcode@gmail.com>
2020-09-15 22:39:02 +0000

    Finished exercise 7

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,7 @@
 class Employee < ActiveRecord::Base
     belongs_to :store
+    validates_associated :store 
+    validates :first_name, :last_name, presence: true
+    validates :hourly_rate, numericality: { only_integer: true }
+    validates :hourly_rate, inclusion: { in: 40..200}
 end
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,8 @@
 class Store < ActiveRecord::Base
     has_many :employees
+    validates_length_of :name, minimun: 3
+    validates :annual_revenue, numericality: {
+        only_integer: true,
+        greater_than_or_equal_to: 0
+    }
 end

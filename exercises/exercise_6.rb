9187c786e68fea5bb6777b9275fd0f8f8be130dc
dreamxinxcode <dreamxinxcode@gmail.com>
2020-09-15 22:06:49 +0000

    Finished exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,10 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Fern", last_name: "Koch", hourly_rate: 30)
+@store1.employees.create(first_name: "Cali", last_name: "Wise", hourly_rate: 50)
+
+@store2.employees.create(first_name: "Delia", last_name: "Goulding", hourly_rate: 60)
+@store2.employees.create(first_name: "Beverley", last_name: "Farley", hourly_rate: 23)
+@store2.employees.create(first_name: "Lyle", last_name: "Hunt", hourly_rate: 60)

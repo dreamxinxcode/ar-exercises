e79b2b7cd21d2dccfaf3eaaf241c3c00d2ab655f
dreamxinxcode <dreamxinxcode@gmail.com>
2020-09-15 21:43:57 +0000

    Finished exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,11 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+revenue_sum = Store.sum(:annual_revenue)
+puts "Revenue: $#{revenue_sum}"
+
+average_revenue = Store.average(:annual_revenue)
+puts "Average revenue: $#{average_revenue}"
+
+over_one_million = Store.where("annual_revenue > 1000000").count
+puts over_one_million
